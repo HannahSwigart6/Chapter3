602d793519e55e2f9d2011fc8ef41486f7aa424b
HannahSwigart6 <hannahdeanswigart@gmail.com>
2017-09-11 09:58:12 -0500

    update

---
+++ Chapter 3/src/YouDoIt/TestInfo.java
@@ -0,0 +1,11 @@
+package YouDoIt;
+
+public class TestInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Calling method from another class:");
+		ParadiseInfo.displayInfo();
+	}
+
+}
---
+++ Chapter 3/src/tutorial/Second.java
@@ -0,0 +1,15 @@
+package tutorial;
+
+public class Second {
+
+	
+		public static void displayAddress()
+		{	
+		System.out.println("XYZ Company");
+		System.out.println("8900 U.S. Hwy 14");
+		System.out.println("Crystal Lake, IL 60014");
+		System.out.println("First Java application");
+		}
+	}
+
+
