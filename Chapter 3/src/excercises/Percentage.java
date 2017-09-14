6f1b0f101c2f8f58fd8f606a32cac4bfaf08a972
HannahSwigart6 <hannahdeanswigart@gmail.com>
2017-09-14 09:29:53 -0500

    update

--- Chapter 3/src/excercises/Percentage.java
+++ Chapter 3/src/excercises/Percentage.java
@@ -1,10 +1,18 @@
 package excercises;
-
+import java.util.Scanner;
 public class Percentage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+ double NumberOne, NumberTwo;
+ float Percentage;
+ 
+ System.out.println("Enter in a number >>");
+ NumberOne = input.nextDouble();
+ 
+ System.out.println("Enter in another number >>")
 	}
 
 }
