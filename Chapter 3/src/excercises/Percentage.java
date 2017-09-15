b366fb1439a4dcad4eb2f4bbecc0f234b02c1fdf
HannahSwigart6 <hannahdeanswigart@gmail.com>
2017-09-15 08:17:49 -0500

    update

--- Chapter 3/src/excercises/Percentage.java
+++ Chapter 3/src/excercises/Percentage.java
@@ -7,12 +7,19 @@ public class Percentage {
 		Scanner input = new Scanner(System.in);
 		
  double NumberOne, NumberTwo;
- float Percentage;
+ float Percentage, Percentage2;
  
  System.out.println("Enter in a number >>");
  NumberOne = input.nextDouble();
  
- System.out.println("Enter in another number >>")
+ System.out.println("Enter in another number >>");
+ NumberTwo = input.nextDouble();
+ 
+ Percentage = (float)((NumberOne * 100) / NumberTwo);
+ Percentage2 = (float)((NumberTwo * 100) / NumberOne);
+ 
+ System.out.println(NumberOne+ " is " + Percentage+ "% of " +NumberTwo);
+ System.out.println(NumberTwo+ " is " + Percentage2+ "% of " +NumberOne);
 	}
 
 }
